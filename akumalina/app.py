845a9ee8b0d2e9c493e4cf05ad4669f781a972b1
Stan <stanislawstepak@outlook.com>
2019-04-27 09:04:20 +0200

    Testing

--- akumalina/app.py
+++ akumalina/app.py
@@ -1,27 +1,21 @@
 from flask import Flask, render_template
-from flask_caching import Cache
+#from flask_caching import Cache
+import os
 import time
 from subprocess import check_output
 
+from file_handling import FileHandler
+from plotting import Plotter
+
 app = Flask(__name__)
-cache = Cache(app,config={'CACHE_TYPE': 'simple'})
+#cache = Cache(app,config={'CACHE_TYPE': 'simple'})
 
 @app.route('/')
-@cache.cached(timeout=50)
 def hello():
-    time.sleep(10)
-    check_output('./temperatures/rysowanie.sh')
+    data = FileHandler().read_temperatures_data()
+    a = Plotter(data)
+    a.create_plot()
     return render_template('main.html')
-
-@app.after_request
-def add_header(response):
-    """
-    Add headers to both force latest IE rendering engine or Chrome Frame,
-    and also to cache the rendered page for 10 minutes.
-    """
-    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
-    response.headers['Cache-Control'] = 'public, max-age=0'
-    return response
 
 
 if __name__ == '__main__':
